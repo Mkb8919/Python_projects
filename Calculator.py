fbab556f200753fbd50033f609258a210f0ad00b
Mohit Bhatia <154704641+Mkb8919@users.noreply.github.com>
2024-08-17 22:54:40 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+# Calculator
+
+# a = int(input("type the first number:"))
+# b = int(input("type the second number:"))
+# print("the sum of the two numbers is:",a+b)
+# print("the difference of the two numbers is:", a-b)
+# print("the product of the two numbers is:", a*b)
+# print("the division of the two numbers is:", a/b)
+
+# print("this is the solution:")
+
+# Another way
+
+a = 50
+b = 3
+
+print("the value of", a, "+", 3, "is", a + b)
+print("the value of", a, "-", 3, "is", a - b)
+print("the value of", a, "*", 3, "is", a * b)
+print("the value of", a, "/", 3, "is", a / b)
+
