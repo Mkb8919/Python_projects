4133728fd57bd48620afd852a090a8e67bcee45e
Mohit Bhatia <154704641+Mkb8919@users.noreply.github.com>
2024-08-20 21:58:45 +0530

    mega project -1 jarvis

---
+++ mega project -jarvis/musicLibrary.py
@@ -0,0 +1,11 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+}
+
+
+
+# "sk-proj-WxS17ehGk2PnwmzCHcDwT3B1bkFJFMj6bYTk9jG1bqZaFTcj"
+# "d093053d72bc40248998159804e0e67d
